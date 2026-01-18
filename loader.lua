b425df49cc61e909b3419b77cc6d158140d906fd
ntkiet09 <nguyentongkiet2009@gmail.com>
2026-01-18 20:09:36 +0700

    dfs
    dfsd

--- loader.lua
+++ loader.lua
@@ -1,5 +1,20 @@
+-- ========= TK HUB KEY SYSTEM =========
+local CORRECT_KEY = "kietsebeo"
+-- ====================================
+
 if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
+if getgenv().TK_KEY == nil then
+    warn("❌ Chưa nhập key!")
+    return
+end
+
+if tostring(getgenv().TK_KEY) ~= CORRECT_KEY then
+    warn("❌ Sai key!")
+    return
+end
+
+-- đúng key -> load main
 loadstring(game:HttpGet("https://raw.githubusercontent.com/TKHubBaric11/cc/main/main.lua"))()
